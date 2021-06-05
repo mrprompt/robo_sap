a627b05c20829f97dc5d0b8ba1b4a726d4abce7c
ederalmeida <eder.almeida@gmail.com>
2021-06-05 20:33:27 -0300

    202106052033

--- sap/efetuar_logon.py
+++ sap/efetuar_logon.py
@@ -4,6 +4,8 @@ from sap import verifica_usario_sap_aberto as vusa
 from janelas import janela_info_sessao_aberta as jisa
 from janelas import janela_logon as jl
 import time
+import PySimpleGUI as sg
+import sys
 
 def efetuar_logon():
     # Verifica se o SAP está no caminho padrão
@@ -18,7 +20,7 @@ def efetuar_logon():
         usar_instancia_aberta = jisa.janela_info_sessao_aberta(vSap[0][1], vSap[0][2])
 
         if usar_instancia_aberta == True:
-            # Caso o usuário responda que quer reutilizar a conexão existente, verifica se a janela SAP está na tela inicial
+            # verifica se a janela SAP está na tela inicial
             if vSap[0][3] == True:
                 objConnection = vSap[0][4]
                 Session = vSap[0][5]
@@ -31,16 +33,22 @@ def efetuar_logon():
                 
             # Caso não tenha uma janela na tela iniciar, cria uma nova janela para a transação
             else:
-                objConnection = vSap[0][4]
-                Session = vSap[0][5]
-                # Criando a nova janela
-                Session.createSession()
-                time.sleep(2)
-                for Session in objConnection.Children:
-                    if Session.Busy == False:
-                        if Session.Info.Transaction == 'SESSION_MANAGER':
-                            return Session
-                            break
+                # Caso o usuário responde que quer reutilizar a conexão existente, verifica se já há 6 janelas abertas
+                #   (o que impossibilita a criação de uma nova). Se houver, encerra a execução
+                if vSap[0][0] == 6:
+                    sg.popup('Este usuário já possui 6 janelas SAP abertas. Impossível abrir uma nova')
+                    sys.exit()
+                else:
+                    objConnection = vSap[0][4]
+                    Session = vSap[0][5]
+                    # Criando a nova janela
+                    Session.createSession()
+                    time.sleep(2)
+                    for Session in objConnection.Children:
+                        if Session.Busy == False:
+                            if Session.Info.Transaction == 'SESSION_MANAGER':
+                                return Session
+                                break
         # Caso o usuário opte por iniciar uma nova instância, abre a tela de Login
         else:
             info_logon = jl.janela_logon()
