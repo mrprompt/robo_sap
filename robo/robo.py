67751768250b3a054f351a23d949480143d15b44
ederalmeida <eder.almeida@gmail.com>
2021-06-17 13:59:01 -0300

    202106171358

--- robo/robo.py
+++ robo/robo.py
@@ -2,7 +2,8 @@ import time
 import pyautogui as pag
 from apoio import verifica_janelas as vj
 from apoio import obter_relacao_contas as orc
-from apoio import verifica_pasta_conta as vp
+from apoio import verifica_pasta_existe as vpe
+from apoio import verifica_pasta_conta as vpc
 from janelas import janela_abertura as ja
 import os
 
@@ -26,52 +27,53 @@ def executa_robo(informacoes_janela_abertura, session):
     contas_conciliaveis = orc.obter_relacao_contas(caminho_arquivo_contas_conciliaveis)
 
     # Abrindo a transação FBL3N
-    session.findById("wnd[0]").iconify()
-    session.findById("wnd[0]").maximize()
-    session.findById("wnd[0]/tbar[0]/okcd").text = "FBL3N"
-    session.findById("wnd[0]").sendVKey(0)
+    session.findById('wnd[0]').iconify()
+    session.findById('wnd[0]').maximize()
+    session.findById('wnd[0]/tbar[0]/okcd').text = 'FBL3N'
+    session.findById('wnd[0]').sendVKey(0)
 
     # Laçõ de repetição, executado para cada conta da relação de contas conciliáveis
     for conta in contas_conciliaveis:
 
         # Insirindo as informações necessárias na tela de Parametros
-        session.findById("wnd[0]/usr/ctxtSD_SAKNR-LOW").text = conta
-        session.findById("wnd[0]/usr/ctxtSD_BUKRS-LOW").text = "ESUL"
-        session.findById("wnd[0]/usr/ctxtPA_STIDA").text = data_referencia
-        session.findById("wnd[0]/usr/chkX_PARK").selected = False
-        session.findById("wnd[0]/usr/ctxtPA_VARI").text = "/MD_CO_SECOG"
+        session.findById('wnd[0]/usr/ctxtSD_SAKNR-LOW').text = conta
+        session.findById('wnd[0]/usr/ctxtSD_BUKRS-LOW').text = 'ESUL'
+        session.findById('wnd[0]/usr/ctxtPA_STIDA').text = data_referencia
+        session.findById('wnd[0]/usr/chkX_PARK').selected = False
+        session.findById('wnd[0]/usr/ctxtPA_VARI').text = '/MD_CO_SECOG'
 
         # tirando print da parametrização
         screenParametrizacao = pag.screenshot()
 
         # executa a geração do relatório
-        session.findById("wnd[0]").sendVKey(8)
+        session.findById('wnd[0]').sendVKey(8)
 
         # Verificando se a execução não teve dados exibidos. Se não houver dados, volta ao inicio do laço
         if session.findById('wnd[0]/sbar').text == 'Nenhuma partida selecionada (ver texto descritivo)':
             # Print resultado
             time.sleep(1)
             screenExecucao = pag.screenshot()
-            pasta_para_salvar_os_arquivos = vp.verifica_pasta_conta(caminho_pasta_relatorios, conta)
-              # Salvando os prints tirados
-            screenParametrizacao.save(pasta_para_salvar_os_arquivos + "/" + mes_referencia + conta + ' 01 parametrizacao.jpg')
-            screenExecucao.save(pasta_para_salvar_os_arquivos + "/" + mes_referencia + conta + ' 02 resultados.png')
+            pasta_para_salvar_os_arquivos = vpc.verifica_pasta_conta(caminho_pasta_relatorios, conta)
+            # Salvando os prints tirados
+            vpe.verifica_pasta_existe(pasta_para_salvar_os_arquivos + '/prints')
+            screenParametrizacao.save(pasta_para_salvar_os_arquivos + '/prints/' + mes_referencia + conta + ' 01 parametrizacao.jpg')
+            screenExecucao.save(pasta_para_salvar_os_arquivos + '/prints' + mes_referencia + conta + ' 02 resultados.png')
             continue
 
         # Capturando informações do razão gerado
         # gerando visão totalizada
-        session.findById("wnd[0]/mbar/menu[1]/menu[10]").select()
-        session.findById("wnd[1]/usr/tblSAPLSKBHTC_WRITE_LIST_820").getAbsoluteRow(0).selected = -1
-        session.findById("wnd[1]/usr/btnB_SEARCH").press()
-        session.findById("wnd[2]/usr/txtGD_SEARCHSTR").text = "Conta"
-        session.findById("wnd[1]/usr/btnAPP_WL_SING").press()
-        session.findById("wnd[1]/tbar[0]/btn[0]").press()
-        session.findById("wnd[0]/usr/lbl[5,8]").setFocus()
-        session.findById("wnd[0]/usr/lbl[5,8]").caretPosition = 7
-        session.findById("wnd[0]/tbar[1]/btn[31]").press()
+        session.findById('wnd[0]/mbar/menu[1]/menu[10]').select()
+        session.findById('wnd[1]/usr/tblSAPLSKBHTC_WRITE_LIST_820').getAbsoluteRow(0).selected = -1
+        session.findById('wnd[1]/usr/btnB_SEARCH').press()
+        session.findById('wnd[2]/usr/txtGD_SEARCHSTR').text = 'Conta'
+        session.findById('wnd[1]/usr/btnAPP_WL_SING').press()
+        session.findById('wnd[1]/tbar[0]/btn[0]').press()
+        session.findById('wnd[0]/usr/lbl[5,8]').setFocus()
+        session.findById('wnd[0]/usr/lbl[5,8]').caretPosition = 7
+        session.findById('wnd[0]/tbar[1]/btn[31]').press()
 
         # Abrindo tela de informações
-        session.findById("wnd[0]").sendVKey(35)
+        session.findById('wnd[0]').sendVKey(35)
               
         # Esperando um segundo, para dar tempo da janela de informações aparecer antes de tirar o print
         time.sleep(1)
@@ -85,19 +87,19 @@ def executa_robo(informacoes_janela_abertura, session):
         screenExecucao = pag.screenshot()
         
         # Fecha a janela
-        session.findById("wnd[0]").sendVKey(0)
+        session.findById('wnd[0]').sendVKey(0)
 
         #Exportanto para Excel
-        pasta_para_salvar_os_arquivos = vp.verifica_pasta_conta(caminho_pasta_relatorios, conta)
-        session.findById("wnd[0]/mbar/menu[0]/menu[3]/menu[1]").select()
-        session.findById("wnd[0]").sendVKey(0)
-        session.findById("wnd[1]/usr/ctxtDY_PATH").text = pasta_para_salvar_os_arquivos + "/"
-        session.findById("wnd[1]/usr/ctxtDY_FILENAME").text = mes_referencia + ' ' + conta + '.xlsx'
-        session.findById("wnd[1]").sendVKey(0)
+        pasta_para_salvar_os_arquivos = vpc.verifica_pasta_conta(caminho_pasta_relatorios, conta)
+        session.findById('wnd[0]/mbar/menu[0]/menu[3]/menu[1]').select()
+        session.findById('wnd[0]').sendVKey(0)
+        session.findById('wnd[1]/usr/ctxtDY_PATH').text = pasta_para_salvar_os_arquivos + '/relatorios'
+        session.findById('wnd[1]/usr/ctxtDY_FILENAME').text = mes_referencia + ' ' + conta + '.xlsx'
+        session.findById('wnd[1]').sendVKey(0)
 
         # mapeando a janela ativa. Quando detectar que é o Excel, mata o processo.
         janela_ativa = ''
-        while janela_ativa != "EXCEL.EXE":
+        while janela_ativa != 'EXCEL.EXE':
             time.sleep(1)
             janela_ativa = vj.verifica_janela_ativa()
 
@@ -111,8 +113,8 @@ def executa_robo(informacoes_janela_abertura, session):
         janela_ativa = ''
 
         # Salvando os prints tirados
-        screenParametrizacao.save(pasta_para_salvar_os_arquivos + "/" + mes_referencia + conta + ' 01 parametrizacao.jpg')
-        screenExecucao.save(pasta_para_salvar_os_arquivos + "/" + mes_referencia + conta + ' 02 resultados.png')
+        screenParametrizacao.save(pasta_para_salvar_os_arquivos + '/' + mes_referencia + conta + ' 01 parametrizacao.jpg')
+        screenExecucao.save(pasta_para_salvar_os_arquivos + '/' + mes_referencia + conta + ' 02 resultados.png')
         
         # Volta para a tela de parâmetros
-        session.findById("wnd[0]").sendVKey(15)
+        session.findById('wnd[0]').sendVKey(15)
